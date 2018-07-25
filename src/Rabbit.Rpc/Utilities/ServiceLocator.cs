30a9bb87a8da6a3f4b4749990559d0bab74872c8
yjy <sunriseyuen@qq.com>
2018-07-25 22:59:38 +0800

    +Utilities

---
+++ src/Rabbit.Rpc/Utilities/ServiceLocator.cs
@@ -0,0 +1,51 @@
+﻿using Autofac;
+using System;
+
+namespace Rabbit.Rpc.Utilities
+{
+    public class ServiceContainer
+    {
+        public static IContainer Current { get; set; }
+
+        public static T Resolve<T>()
+        {
+            return Current.Resolve<T>();
+        }
+
+        public static bool IsRegistered<T>()
+        {
+            return Current.IsRegistered<T>();
+        }
+
+        public static bool IsRegistered<T>(string key)
+        {
+            return Current.IsRegisteredWithKey<T>(key);
+        }
+
+        public static bool IsRegistered(Type type)
+        {
+            return Current.IsRegistered(type);
+        }
+
+        public static bool IsRegisteredWithKey(string key, Type type)
+        {
+            return Current.IsRegisteredWithKey(key, type);
+        }
+
+        public static T ResolveKeyed<T>(string key)
+        {
+       
+            return Current.ResolveKeyed<T>(key);
+        }
+
+        public static object Resolve(Type type)
+        {
+            return Current.Resolve(type);
+        }
+
+        public static object ResolveKeyed(string key, Type type)
+        {
+            return Current.ResolveKeyed(key, type);
+        }
+    }
+}
