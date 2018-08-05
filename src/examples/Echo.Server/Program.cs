67c499db8bcb3f6518b37fa3623bc9111dbec062
yjy <sunriseyuen@qq.com>
2018-08-05 12:15:29 +0800

    -

--- src/examples/Echo.Server/Program.cs
+++ src/examples/Echo.Server/Program.cs
@@ -2,7 +2,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Rabbit.Rpc;
-using Rabbit.Rpc.Address;
+//using Rabbit.Rpc.Address;
 using Rabbit.Rpc.Routing;
 using Rabbit.Rpc.Runtime.Server;
 using System;
@@ -49,10 +49,7 @@ namespace Echo.Server
                 var serviceEntryManager = serviceProvider.GetRequiredService<IServiceEntryManager>();
                 var addressDescriptors = serviceEntryManager.GetEntries().Select(i => new ServicePath
                 {
-                    Address = new[]
-                    {
-                        new IpAddressModel { Ip = "127.0.0.1", Port = 9981 }
-                    },
+                    Address =  new string[] { "127.0.0.1:9981" },
                     ServiceEntry = i
                 });
 
