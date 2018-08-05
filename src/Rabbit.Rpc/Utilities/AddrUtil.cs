85f0bf1469fd8cdcd13cd86c08cae47563b63c32
yjy <sunriseyuen@qq.com>
2018-08-05 12:10:13 +0800

    -

---
+++ src/Rabbit.Rpc/Utilities/AddrUtil.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace Rabbit.Rpc.Utilities
+{
+    public class AddrUtil
+    {
+        public static IPEndPoint CreateEndPoint(string address)
+        {
+         
+            var addr = address.Split(':');
+            IPAddress IPadr = IPAddress.Parse(addr[0]);//先把string类型转换成IPAddress类型
+            IPEndPoint EndPoint = new IPEndPoint(IPadr, int.Parse(addr[1]));//传递IPAddress和Port
+            return EndPoint;
+        }
+      
+    }
+}
