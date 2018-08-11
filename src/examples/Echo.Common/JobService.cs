54aba4cdce39b3b0c93e169815722bba1dd98dd6
yjy <sunriseyuen@qq.com>
2018-08-11 09:27:03 +0800

    -

---
+++ src/examples/Echo.Common/JobService.cs
@@ -0,0 +1,68 @@
+﻿using Rabbit.Rpc.Runtime.Server.Implementation.ServiceDiscovery.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Jacob.Common
+{
+
+    [ServiceMetadata("WaitExecution", true)]
+    public class JobService : IUserService
+    {
+        #region Implementation of IUserService
+
+        public Task<string> GetUserName(int id)
+        {
+            return Task.FromResult($"id:{id} is name rabbit.");
+        }
+
+        public Task<bool> Exists(int id)
+        {
+            return Task.FromResult(true);
+        }
+
+        public Task<int> GetUserId(string userName)
+        {
+            return Task.FromResult(1);
+        }
+
+        public Task<DateTime> GetUserLastSignInTime(int id)
+        {
+            return Task.FromResult(DateTime.Now);
+        }
+
+        public Task<UserModel> GetUser(int id)
+        {
+            // Console.Write(".");
+            return Task.FromResult(new UserModel
+            {
+                Name = "rabbit-" + id.ToString(),
+                Age = id
+            });
+        }
+        public Task<bool> Update(int id, UserModel model)
+        {
+            return Task.FromResult(true);
+        }
+
+        public Task<IDictionary<string, string>> GetDictionary()
+        {
+            return Task.FromResult<IDictionary<string, string>>(new Dictionary<string, string> { { "key", "value" } });
+        }
+
+        public async Task Try()
+        {
+            Console.WriteLine("start");
+            await Task.Delay(5000);
+            Console.WriteLine("end");
+        }
+
+        public Task TryThrowException()
+        {
+            throw new Exception("用户Id非法！");
+        }
+
+        #endregion Implementation of IUserService
+    }
+
+}
